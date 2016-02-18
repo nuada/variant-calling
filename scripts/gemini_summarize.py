d0cee19d792b1a4bca42f4a1c52c6ea01d55d460
Piotr Radkowski <piotr.radkowski@uj.edu.pl>
2016-02-18 18:35:25 +0100

    Fix for Gemini 0.18.0

--- scripts/gemini_summarize.py
+++ scripts/gemini_summarize.py
@@ -18,17 +18,17 @@ gq.run(sys.argv[1], None, True)
 header_printed = False
 genotype_columns = [ 'gt_depths', 'gt_ref_depths', 'gt_alt_depths', 'gts', 'gt_quals' ]
 for row in gq:
-	columns = row.row.keys()[:-3]
+	columns = row.row.keys()[:-11]
 	if (not header_printed):
 		# Print file header
 		print '\t'.join(['SAMPLE_ID'] + [ s[:-1] for s in genotype_columns ] + columns)
 		header_printed = True
 
 	# Output only het & hom alt variants
-	for sample in row.variant_samples:
+	for sample in [item for sublist in row.genotype_dict for item in sublist]:
 		# Drop low depth and low quality variants
 		if row['gt_depths'][gq.sample2index[sample]] < DP_THRESHOLD or row['gt_quals'][gq.sample2index[sample]] < GQ_THRESHOLD: continue
 
 		# Print selected genotype and varinat columns
-		genotype = [ str(row[col][gq.sample2index[sample]]) for col in genotype_columns if col in row.gt_cols ]
+		genotype = [ str(row[col][gq.sample2index[sample]]) for col in genotype_columns ]
 		print '\t'.join([sample] + genotype + [ str(row[col]) for col in columns ])
